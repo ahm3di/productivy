90a4ae20ed90818dec0c2afc08a002423ed73f69
ahm3di <ahm3diqbal@gmail.com>
2021-04-16 19:12:54 +0100

    Add base template

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('base.html')
 
-@app.route('/about')
-def about():
-    return "About"
+
+if __name__ == "__main__":
+    db.create_all()
+
+    app.run(debug=True)
