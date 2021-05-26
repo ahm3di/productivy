e9ff951a137e96d439eaef385133a64b7581b30e
ahm3di <ahm3diqbal@gmail.com>
2021-05-26 01:54:25 +0100

    Add user authentication Add register form Add login form Add fomantic styling to login form Add validation to login form

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
 ﻿from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secretkeygoeshere'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)])
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)])
+
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_name = User.query.filter_by(
+            username=username.data).first()
+
+        if existing_user_name:
+            raise ValidationError("Username already exists")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)])
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)])
+    remember = BooleanField('remember me')
+
+    submit = SubmitField("Login")
 
 
 class Todo(db.Model):
@@ -13,7 +63,41 @@ class Todo(db.Model):
     complete = db.Column(db.BOOLEAN)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Login
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Register
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index.html'))
+    return render_template('register.html', form=form)
+
+
 @app.route('/')
+@login_required
 def index():
     # Show all todos
     todo_list = Todo.query.all()
@@ -21,6 +105,7 @@ def index():
 
 
 @app.route("/add", methods=["POST"])
+@login_required
 def add():
     # Add new todo
     title = request.form.get("title")
@@ -31,6 +116,7 @@ def add():
 
 
 @app.route("/update_status/<int:todo_id>")
+@login_required
 def update_status(todo_id):
     # Update todo status
     todo = Todo.query.filter_by(id=todo_id).first()
@@ -40,6 +126,7 @@ def update_status(todo_id):
 
 
 @app.route("/delete/<int:todo_id>")
+@login_required
 def delete(todo_id):
     # Delete todo
     todo = Todo.query.filter_by(id=todo_id).first()
@@ -49,6 +136,7 @@ def delete(todo_id):
 
 
 @app.route("/update/<int:todo_id>", methods=["POST", "GET"])
+@login_required
 def update(todo_id):
     # Update todo
     todo = Todo.query.filter_by(id=todo_id).first()
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,42 @@
-$(function(){
+$(function () {
 
     //Form validation to prevent empty todos
     $('#todo-input, #update-todo-input').form({
-    fields: {
-      title: 'empty',
-    },
-  });
+        fields: {
+            title: 'empty',
+        },
+    });
+
+    $('.ui.form')
+        .form({
+            fields: {
+                email: {
+                    identifier: 'username',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: 'Please enter your e-mail'
+                        },
+                        {
+                            type: 'email',
+                            prompt: 'Please enter a valid e-mail'
+                        }
+                    ]
+                },
+                password: {
+                    identifier: 'password',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: 'Please enter your password'
+                        },
+                        {
+                            type: 'length[4]',
+                            prompt: 'Your password must be at least 4 characters'
+                        }
+                    ]
+                }
+            }
+        });
 
 });
