f885ac8f70f57f232d596cbff34e7eb631d0b52c
ahm3di <ahm3diqbal@gmail.com>
2021-05-31 07:49:17 +0100

    Add relationships between models Create Project model Add One-to-Many relationship between Project and Todo Add Many-to-Many relationship between User and Project Add route to create new Project Add route to display all todos from a Project

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, \
     logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
 
@@ -22,14 +22,6 @@ login_manager.login_view = "login"
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-class User(db.Model, UserMixin):
-    # Define User model
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
 
 
 def validate_user(username, email):
@@ -65,15 +57,43 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember me')
 
 
+# Association table between User and Project
+user_project = db.Table('user_project',
+                        db.Column('user_id', db.Integer,
+                                  db.ForeignKey('user.id')),
+
+                        db.Column('project_id', db.Integer,
+                                  db.ForeignKey('project.id'))
+                        )
+
+
+class User(db.Model, UserMixin):
+    # Define User model
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    Projects = db.relationship('Project', secondary=user_project,
+                               backref=db.backref('users'))
+
+
 class Todo(db.Model):
     # Define Todo model
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     complete = db.Column(db.BOOLEAN)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    todos = db.relationship('Todo', backref='project')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+
     # Register user
     form = RegisterForm()
     if form.validate_on_submit():
@@ -114,17 +134,42 @@ def logout():
 @app.route('/')
 @login_required
 def index():
-    # Show all todos
-    todo_list = Todo.query.all()
-    return render_template('index.html', todo_list=todo_list)
+    # Show all projects for current user
+    projects = Project.query.filter(Project.users.any(id=current_user.id)).all()
+
+    return render_template('index.html', projects=projects)
 
 
-@app.route("/add", methods=["POST"])
+@app.route('/project/<int:project_id>')
 @login_required
-def add():
+def project(project_id):
+    # Show all todos from project
+    todo_list = Todo.query.filter_by(project_id=project_id).all()
+    print(todo_list)
+    return render_template('project.html', project_id=project_id,
+                           todo_list=todo_list)
+
+
+@app.route("/add_project/", methods=["POST"])
+@login_required
+def add_project():
+    # Add new Project
+    name = request.form.get("name")
+    new_project = Project(name=name)
+    current_user.Projects.append(new_project)
+    db.session.add(new_project)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/add_todo/<int:project_id>", methods=["POST"])
+@login_required
+def add_todo(project_id):
     # Add new todo
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
+    current_project = Project.query.filter_by(id=project_id).first()
+    current_project.todos.append(new_todo)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("index"))
