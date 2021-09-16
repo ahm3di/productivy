8c9585c5ec178b2ec4d55e17d7b0b61c626a3441
ahm3di <ahm3diqbal@gmail.com>
2021-09-16 02:03:15 +0100

    Fix heroku dialect error

--- config.py
+++ config.py
@@ -4,11 +4,13 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DATABASE_URL = os.environ.get("DATABASE_URL").replace("postgres",
+                                                          "postgresql")
     UPLOAD_FOLDER = 'static/profile_images'
 
 
@@ -18,8 +20,5 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     UPLOAD_FOLDER = 'static/profile_images'
     SEND_FILE_MAX_AGE_DEFAULT = 0
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     TESTING = True
-
-
