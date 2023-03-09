433f740b06772d2e6080f59d41e92534673a8b24
Ahm3di <ahm3diqbal@gmail.com>
2023-03-09 13:57:05 +0000

    Replace eventlet with gevent due to security vulnerability

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, user_id)
 
 
 # Used to convert UTC time to GMT and inject values in jinja2 template
